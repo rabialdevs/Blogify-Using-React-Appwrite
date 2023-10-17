6368aef56af6e4285e46878b6771f941b721d245
rabialhosen404 <rabialhosen404@gmail.com>
2023-10-17 23:19:22 +0530

    build v1.0.1

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div>
+      <img src="/logo.svg" alt="Logo" className='w-24 h-10 rounded-full'/>
+    </div>
   )
 }
 
